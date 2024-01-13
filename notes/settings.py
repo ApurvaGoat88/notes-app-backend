b9db83f99fac8c2a95c88ceeba7f7dfeb5d20f0c
ApurvaGoat88 <apoorvbrajQgmail.com>
2024-01-13 22:22:16 +0530

    allowed host

--- notes/settings.py
+++ notes/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4sz7fhn5#z&*!kfhrkeimwlv%bla7$7_qgn#jo54ibv1f8vtmc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
